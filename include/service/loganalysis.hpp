8745e2217198455ba8696544d8582fa2546b6f91
Pravin Kumar <pravinkumar.m@hcl.com>
2023-08-21 20:41:42 +0530

    Created four functions to view the mathed logs

--- include/service/loganalysis.hpp
+++ include/service/loganalysis.hpp
@@ -11,13 +11,19 @@ struct P_RULE
 {
     int id;
     int if_sid;
+    int if_mid;
     int same_source_ip;
+    int same_id;
     int time;
     std::time_t start;
     std::time_t end;
     int frequency;
     int d_frequency;
     string src_ip;
+    string group;
+
+    P_RULE() : id(0), if_sid(0), if_mid(0), same_source_ip(0),
+               same_id(0), time(0), frequency(0), d_frequency(0) {}
 };
 
 class LogAnalysis
@@ -28,6 +34,7 @@ private:
     vector<P_RULE> _processingRules;
     vector<int> _processedRules;
     vector<int> _idRules;
+    map<string, map<int, AConfig>> _rules;
 
 private:
     int isRuleFound(const int ruleId);
@@ -49,13 +56,29 @@ public:
 
     int regexMatch(const string log, const string pattern);
 
-    int pcreMatch(const string input, const string pattern);
-    
-    int match(LOG_EVENT &logInfo,map<string, map<int, AConfig>> rules);
+    int pcreMatch(const std::string& input, const std::string& pattern);
+    /*
+        read the rules one by one
+        checking if any rules in the memory, which is expired if it is remove it,
+        checking any rules is processing state wait for logs
+        Checking and do the regex matching for the logs
+        Checking the read syslog size.
+        Checking any rule which have child id, that might be identified before.
+        Checking the timeframe or frequency is available for the rule if it is, do the given.
+        And during those checkings if rule matched add the rule id to the Id'd rules.
+    */
+    int match(LOG_EVENT &logInfo);
 
     int analyseFile(const string file, const string format);
 
     int start(const string path);
+
+    int postAnalysis(const vector<LOG_EVENT> alerts);
+
+    AConfig getRule(const string group,const int ruleId);
+
+    int printLogDetails(AConfig ruleInfo, LOG_EVENT logInfo);
+
 
     ~LogAnalysis(){}
 };
