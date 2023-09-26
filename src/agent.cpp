41ed205aa5eec0576a32167979f923fb74118a58
Pravin Kumar <pravinkumar.m@hcl.com>
2023-09-26 14:09:04 +0530

    Tested application

--- src/agent.cpp
+++ src/agent.cpp
@@ -1,15 +1,16 @@
-// #include "controller/mainController.hpp"
+#include "controller/mainController.hpp"
+#include "agentUtils.hpp"
 
-// int main()
-// {
-//     openlog("agent", LOG_INFO | LOG_CONS, LOG_USER);
-        // AgentUtils::logfp.open(LOG_PATH, std::ios::app);
-//     MainController controller(BASE_CONFIG_DIR);
-//     controller.start();
-        // if (AgentUtils::logfp.is_open())
-        // {
-        //     AgentUtils::logfp.close();
-        // }
-//     closelog();
-//     return 0;
-// }
+int main()
+{
+    openlog("agent", LOG_INFO | LOG_CONS, LOG_USER);
+        AgentUtils::logfp.open(LOG_PATH, std::ios::app);
+    MainController controller(BASE_CONFIG_DIR);
+    controller.start();
+        if (AgentUtils::logfp.is_open())
+        {
+            AgentUtils::logfp.close();
+        }
+    closelog();
+    return 0;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,39 +1,47 @@
-#include "service/fwservice.hpp"
-#include "service/configservice.hpp"
-#include "service/rqueue.hpp"
+// #include "service/fwservice.hpp"
+// #include "service/configservice.hpp"
+// #include "service/rqueue.hpp"
 #include "agentUtils.hpp"
-#include "controller/logController.hpp"
-#include "controller/monitorController.hpp"
-#include "service/loganalysis.hpp"
-#include "controller/monitorController.hpp"
+#include "service/commandservice.hpp"
+// #include "controller/logController.hpp"
+// #include "controller/monitorController.hpp"
+// #include "service/loganalysis.hpp"
+// #include "controller/monitorController.hpp"
 
-int main()
-{
-    AgentUtils::syslog_enabled = true;
-    AgentUtils::setupLogger();
-    if (!AgentUtils::syslog_enabled)
-    {
-        AgentUtils::logfp.open(LOG_PATH, std::ios::app);
-    }
-    Timer timer;
-    Config config;
-    // MonitorController controller;
-    // map<string, map<string, string>> table;
-    // config.readIniConfigFile("/home/krishna/security/Agent/config/rules.config", table);
-    // controller.getMonitorLog(table);
-    LogAnalysis logAnalysis;
-    std::unordered_map<string, std::unordered_map<int, AConfig>> table;
-    std::unordered_map<string, decoder> dTable;
-    logAnalysis.start("/home/krishna/security/Agent/decoder.xml", "/home/krishna/security/Agent/rules/syslog_rules.xml", "/home/krishna/security/Agent/config/dpkg.log");
-    // Queue queue(
-    //     "/home/krishna/Downloads/rabbit-keys/keys/ca/ca_cert.pem",
-    //     "/home/krishna/Downloads/rabbit-keys/keys/server/server_cert.pem",
-    //     "/home/krishna/Downloads/rabbit-keys/keys/server/private/server_key.pem");
-    // queue.send("/home/krishna/security/Agent/process.json", "monitorQueue");
+// int main()
+// {
+//     Command command;
+//     vector<string> logs;
+//     command.readCommand("aa-status",logs);
+//     for(string line: logs){ cout << line << "\n";}
+// }
+// int main()
+// {
+//     AgentUtils::syslog_enabled = true;
+//     AgentUtils::setupLogger();
+//     if (!AgentUtils::syslog_enabled)
+//     {
+//         AgentUtils::logfp.open(LOG_PATH, std::ios::app);
+//     }
+//     Timer timer;
+//     Config config;
+//     // MonitorController controller;
+//     // map<string, map<string, string>> table;
+//     // config.readIniConfigFile("/home/krishna/security/Agent/config/rules.config", table);
+//     // controller.getMonitorLog(table);
+//     LogAnalysis logAnalysis;
+//     std::unordered_map<string, std::unordered_map<int, AConfig>> table;
+//     std::unordered_map<string, decoder> dTable;
+//     logAnalysis.start("/home/krishna/security/Agent/decoder.xml", "/home/krishna/security/Agent/rules/syslog_rules.xml", "/home/krishna/security/Agent/config/dpkg.log");
+//     // Queue queue(
+//     //     "/home/krishna/Downloads/rabbit-keys/keys/ca/ca_cert.pem",
+//     //     "/home/krishna/Downloads/rabbit-keys/keys/server/server_cert.pem",
+//     //     "/home/krishna/Downloads/rabbit-keys/keys/server/private/server_key.pem");
+//     // queue.send("/home/krishna/security/Agent/process.json", "monitorQueue");
     
-    if (AgentUtils::logfp.is_open())
-    {
-        AgentUtils::logfp.close();
-    }
-    return 0;
-}
+//     if (AgentUtils::logfp.is_open())
+//     {
+//         AgentUtils::logfp.close();
+//     }
+//     return 0;
+// }
